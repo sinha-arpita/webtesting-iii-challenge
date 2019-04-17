da2e5d4084bb1faada9f82b9c9a0ea0907412619
Arpita Sinha <sinha.arpita@gmail.com>
2019-04-16 20:05:42 -0700

    MVP done with testing ofDisplay and options

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Display from '../display/Display';
-import Controls from '../controls/Controls';
+import Display from "../display/Display";
+import Controls from "../controls/Controls";
 
 class Dashboard extends React.Component {
   state = {
     locked: false,
-    closed: false,
+    closed: false
   };
 
   render() {
-    const { closed, locked } = this.state;
+    const { locked, closed } = this.state;
 
     return (
       <>
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,102 @@
 // Test away
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import Controls from "./../controls/Controls";
+import Display from "./../display/Display";
+
+it("Checks if close button is called", () => {
+  const mockFn = jest.fn();
+  const { getByText } = render(<Controls toggleClosed={mockFn} />);
+
+  const closeButton = getByText("Close Gate");
+
+  fireEvent.click(closeButton);
+
+  expect(mockFn).toHaveBeenCalled();
+});
+
+it("Checks if open button is called", () => {
+  const mockFn = jest.fn();
+  const { getByText } = render(
+    <Controls closed={true} toggleClosed={mockFn} />
+  );
+
+  const openButton = getByText("Open Gate");
+
+  fireEvent.click(openButton);
+
+  expect(mockFn).toHaveBeenCalled();
+});
+
+it("Checks if unlock button is called", () => {
+  const mockFn = jest.fn();
+  const { getByText } = render(
+    <Controls closed={true} locked={true} toggleLocked={mockFn} />
+  );
+
+  const unlockButton = getByText("Unlock Gate");
+
+  fireEvent.click(unlockButton);
+
+  expect(mockFn).toHaveBeenCalled();
+});
+
+xit("Checks if lock button is called", () => {
+  const mockFn2 = jest.fn();
+
+  const { getByText } = render(
+    <Controls closed={true} locked={false} toggleLocked={mockFn2} />
+  );
+
+  const lockButton = getByText("Lock Gate");
+
+  fireEvent.click(lockButton);
+
+  expect(mockFn2).toHaveBeenCalled();
+});
+
+it("Checks closed is diplayed and gate is unlocked", () => {
+  const { getByText } = render(<Display closed={true} locked={false} />);
+
+  const text = getByText("Closed");
+  const text2 = getByText("Unlocked");
+
+  expect(text).toBeInTheDocument();
+  expect(text2).toBeInTheDocument();
+});
+
+it("Checks closed is diplayed  and door is locked", () => {
+  const { getByText } = render(<Display closed={true} locked={true} />);
+
+  const text = getByText("Closed");
+  const text2 = getByText("Locked");
+
+  expect(text).toBeInTheDocument();
+  expect(text2).toBeInTheDocument();
+});
+it("Checks open is displayed and door is unlocked", () => {
+  const { getByText } = render(<Display opened={true} unlocked={true} />);
+  const text = getByText("Open");
+  const text2 = getByText("Unlocked");
+  expect(text).toBeInTheDocument();
+  expect(text2).toBeInTheDocument();
+});
+it("Checks closed is displayed and door is unlocked", () => {
+  const { getByText } = render(<Display close={true} unlocked={true} />);
+  const text = getByText("Closed");
+  const text2 = getByText("Unlocked");
+  expect(text).toBeInTheDocument();
+  expect(text2).toBeInTheDocument();
+});
+
+it("Checks if buttons are there", () => {
+  const { queryByText } = render(<Controls />);
+
+  const closeButton = queryByText("Close Gate");
+
+  expect(closeButton).toBeInTheDocument();
+});
+
+
